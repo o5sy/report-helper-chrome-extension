6d8823edcb4f8fa5abe68d3f7bbdfc618e5a9cfe
o5sy <oosydev@gmail.com>
2025-07-18 18:51:46 +0900

    Feat: Button 컴포넌트 추가

--- src/popup/components/ui/button.tsx
+++ src/popup/components/ui/button.tsx
@@ -5,7 +5,7 @@ function Button({
   ...props
 }: PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>>) {
   const { className, ...rest } = props;
-  const classes = `transition-colors hover:bg-[#46aa32]/90 rounded-md bg-primary px-4 py-2 text-primary-foreground focus-visible:ring-ring disabled:opacity-50 ${className}`;
+  const classes = `transition-colors disabled:bg-gray-400 hover:bg-[#46aa32]/90 rounded-md bg-primary px-4 py-2 text-primary-foreground focus-visible:ring-ring disabled:opacity-50 ${className}`;
 
   return (
     <button className={classes} type="button" {...rest}>
