5649b9992c50aec78793bf76f529825dca50f2e7
o5sy <oosydev@gmail.com>
2025-12-23 23:10:46 +0900

    fix: hmr 모드에서 dev server 실행안되는 오류 수정
    원인1. background 스크립트에서 window 객체에 접근
    원인2. content_scripts.matches 값
    
    TODO
    일단 원인만 파악한 거라 matches 값은 추후 찾아보고 수정할 예정

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,14 @@
-import { crx } from "@crxjs/vite-plugin";
-import { defineConfig } from "vite";
-import manifest from "./manifest.json";
-import path from "node:path";
-import react from "@vitejs/plugin-react";
+import { crx } from '@crxjs/vite-plugin';
+import { defineConfig } from 'vite';
+import manifest from './manifest.json';
+import path from 'node:path';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   resolve: {
     alias: {
-      "@": `${path.resolve(__dirname, "src")}`,
+      '@': `${path.resolve(__dirname, 'src')}`,
     },
   },
   plugins: [react(), crx({ manifest })],
-  server: {
-    cors: {
-      origin: [/chrome-extension:\/\//],
-    },
-  },
 });
